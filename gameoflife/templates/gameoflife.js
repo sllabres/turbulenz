88f6b17943638c55975b7b994f2faa11785c61e9
Seb <sllabres@gmail.com>
2012-11-15 13:25:34 +0000

    Automated Commit

---
+++ gameoflife/templates/gameoflife.js
@@ -0,0 +1,35 @@
+/*{# Copyright (c) 2010-2012 Turbulenz Limited #}*/
+
+/*{{ javascript("jslib/vmath.js") }}*/
+/*{{ javascript("jslib/aabbtree.js") }}*/
+/*{{ javascript("jslib/camera.js") }}*/
+/*{{ javascript("jslib/geometry.js") }}*/
+/*{{ javascript("jslib/material.js") }}*/
+/*{{ javascript("jslib/light.js") }}*/
+/*{{ javascript("jslib/scenenode.js") }}*/
+/*{{ javascript("jslib/scene.js") }}*/
+/*{{ javascript("jslib/vmath.js") }}*/
+/*{{ javascript("jslib/effectmanager.js") }}*/
+/*{{ javascript("jslib/shadermanager.js") }}*/
+/*{{ javascript("jslib/texturemanager.js") }}*/
+/*{{ javascript("jslib/renderingcommon.js") }}*/
+/*{{ javascript("jslib/defaultrendering.js") }}*/
+/*{{ javascript("jslib/resourceloader.js") }}*/
+/*{{ javascript("jslib/observer.js") }}*/
+/*{{ javascript("jslib/utilities.js") }}*/
+/*{{ javascript("jslib/vertexbuffermanager.js") }}*/
+/*{{ javascript("jslib/indexbuffermanager.js") }}*/
+/*{{ javascript("jslib/requesthandler.js") }}*/
+/*{{ javascript("jslib/services/turbulenzservices.js") }}*/
+/*{{ javascript("jslib/services/gamesession.js") }}*/
+/*{{ javascript("jslib/services/mappingtable.js") }}*/
+/*{{ javascript("jslib/services/turbulenzbridge.js") }}*/
+
+/*{{ javascript("scripts/sampleappmain.js") }}*/
+/*{{ javascript("scripts/sceneloader.js") }}*/
+/*{{ javascript("scripts/motion.js") }}*/
+
+/*{% if tz_development and not tz_canvas %}*/
+/*{# Include JSON implementation for browsers without native support #}*/
+/*{{ javascript("scripts/json2.js") }}*/
+/*{% endif %}*/
