348afc31527c0dd9020a33c1a4f68d64a7174491
Seb <sllabres@gmail.com>
2012-11-15 16:07:57 +0000

    Automated Commit

--- gameoflife/scripts/GameOfLife.js
+++ gameoflife/scripts/GameOfLife.js
@@ -4,10 +4,17 @@
 /*{{ javascript('jslib/floor.js') }}*/
 
 /*jslint browser: true*/
-/*global TurbulenzEngine*/
+/*global TurbulenzEngine,Camera,Floor*/
 TurbulenzEngine.onload = function onload() {
     "use strict";
-    var graphicsDevice = TurbulenzEngine.createGraphicsDevice({});
+    var graphicsDevice = TurbulenzEngine.createGraphicsDevice({}),
+		mathsDevice = TurbulenzEngine.createMathDevice({}),
+		camera = Camera.create(mathsDevice),
+		floor = Floor.create(graphicsDevice, mathsDevice);
+
+	camera.lookAt(mathsDevice.v3BuildZero(),
+	              mathsDevice.v3Build(0, 1, 0),
+	              mathsDevice.v3Build(0, 20, 100));
 
     function tick() {
         if (graphicsDevice.beginFrame()) {
