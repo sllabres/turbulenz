ea67796e5cee16a5dfe95f0f332a8865d2ff67cd
Seb <sllabres@gmail.com>
2012-11-15 12:34:55 +0000

    Automated Commit

--- gameoflife/rakefile.rb
+++ gameoflife/rakefile.rb
@@ -35,6 +35,7 @@ task :makehtml do
 		end
 	end
 
+	puts `maketzjs -t . -o gameoflife.plugin.tzjs #{game_files} --use-strict`
 	puts `makehtml -t . #{game_files} -o gameoflife.plugin.debug.html --use-strict`
 end
 
