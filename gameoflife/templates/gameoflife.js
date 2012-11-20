c1fbdf0cc5b2e81e59e7d06c11368b1d49b42f8a
Seb <sllabres@gmail.com>
2012-11-20 21:22:07 +0000

    Automated Commit

--- gameoflife/templates/gameoflife.js
+++ gameoflife/templates/gameoflife.js
@@ -1,6 +1,5 @@
 /*{# Copyright (c) 2010-2012 Turbulenz Limited #}*/
 
-/*{{ javascript("jslib/services/gamesession.js") }}*/
 /*{{ javascript("jslib/services/mappingtable.js") }}*/
 /*{{ javascript("jslib/draw2d.js") }}*/
 
