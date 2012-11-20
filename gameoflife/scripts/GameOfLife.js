64dd8ac40d23afb15ea972e8f38e2746e865338b
Seb <sllabres@gmail.com>
2012-11-20 20:36:19 +0000

    Automated Commit

--- gameoflife/scripts/GameOfLife.js
+++ gameoflife/scripts/GameOfLife.js
@@ -32,5 +32,5 @@ TurbulenzEngine.onload = function onload() {
 	};
 
     TurbulenzEngine.setInterval(update, 1000 / 60);
-    TurbulenzEngine.setInterval(grid.update, 250);
+    TurbulenzEngine.setInterval(grid.update, 150);
 };
