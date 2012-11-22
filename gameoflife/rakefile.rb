de5fa9f7cadf319dfe2626680926ae617ee2ca63
Seb <sllabres@gmail.com>
2012-11-22 21:36:02 +0000

    Automated Commit

--- gameoflife/rakefile.rb
+++ gameoflife/rakefile.rb
@@ -3,7 +3,7 @@ require 'peach'
 
 task :default => [:build, :git_commit_and_push]
 
-multitask :build => [:javascript_test, :jslint, :makehtml]
+multitask :build => [:javascript_test, :jslint, :makejs]
 
 # set path value for phantomjs
 task :javascript_test do
@@ -17,9 +17,11 @@ task :jslint do
 end
 
 # using python.exe in the turbulenz env/script directory, added to the windows path environmental variable
-task :makehtml do
+task :makejs do
 	sh "maketzjs --mode plugin -t templates -t . -o gameoflife.plugin.tzjs gameoflife.js"
+	#sh "uglifyjs -o gameoflife.plugin.tzjs gameoflife.plugin.tzjs"
 	sh "maketzjs --mode canvas -t templates -t . -o gameoflife.canvas.js gameoflife.js"
+	#sh "uglifyjs -o gameoflife.canvas.js gameoflife.canvas.js"
 end
 
 task :git_commit_and_push do
