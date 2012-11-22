da7b9b2025ff0991e0e86e14569c990063fe1bbd
Seb <sllabres@gmail.com>
2012-11-22 21:14:47 +0000

    Automated Commit

--- gameoflife/tests/CellDrawingTests.js
+++ gameoflife/tests/CellDrawingTests.js
@@ -76,37 +76,37 @@
 	    equal(yCoordinate, 1);
 	});
 
-	/*
-
-	
-
 	test("When cell drawing for cell at index 1 with cell width of 10, then x coordinate is 10", function() {
 	    var cellIndex = 1,
+	    	cellWidth = 10,
 	        gridWidth = 3,
-	        cellWidth = 10,
+	        liveCell = true,
 	        xCoordinate,
-	        context = { fillRect : function (x, y, width, height) {
-	            xCoordinate = x;
-	        } },
-	        cellDrawing = new CellDrawing(context, gridWidth, cellWidth);
+	        boxDrawingStub = { draw : function (drawItem) {
+	            	xCoordinate = drawItem.destinationRectangle[0];
+	        	} 
+	    	},
+	        cellDrawing = new CellDrawing(boxDrawingStub, gridWidth, cellWidth);
 
-	        cellDrawing.draw(true, cellIndex);
+	    cellDrawing.draw(liveCell, cellIndex);
 
 	    equal(xCoordinate, 10);
 	});
 
 	test("When cell drawing for cell at index 3 with cell width of 10, then y coordinate is 10", function() {
 	    var cellIndex = 3,
+	    	cellWidth = 10,
 	        gridWidth = 3,
-	        cellWidth = 10,
+	        liveCell = true,
 	        yCoordinate,
-	        context = { fillRect : function (x, y, width, height) {
-	            yCoordinate = y;
-	        } },
-	        cellDrawing = new CellDrawing(context, gridWidth, cellWidth);
+	        boxDrawingStub = { draw : function (drawItem) {
+	            	yCoordinate = drawItem.destinationRectangle[1];
+	        	} 
+	    	},
+	        cellDrawing = new CellDrawing(boxDrawingStub, gridWidth, cellWidth);
 
-	        cellDrawing.draw(true, cellIndex);
+	    cellDrawing.draw(liveCell, cellIndex);
 
 	    equal(yCoordinate, 10);
-	});*/
+	});
 }());
