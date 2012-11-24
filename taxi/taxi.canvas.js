a9a7b8da82260f1444968bc12cadb9378be0e68c
Seb <sllabres@gmail.com>
2012-11-24 23:14:29 +0000

    Automated Commit

--- taxi/taxi.canvas.js
+++ taxi/taxi.canvas.js
@@ -34009,11 +34009,7 @@ TurbulenzEngine.onload = function onload() {
 				graphicsDevice.createTexture({ src : table.getURL("textures/taxi.png"),
 					mipmaps : true,
 					onload : function (texture) {
-						sprite = Draw2DSprite.create({
-						width : 67,
-						height : 22,
-						origin : [67 / 2, 22 / 2],
-						textureRectangle : [0, 0, 67, 22],
+						sprite = Draw2DSprite.create({						
 						texture : texture
 						});
 						TurbulenzEngine.setInterval(update, 1000 / 60);
