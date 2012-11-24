db86dceb93e60e60cba982c7cebec30c1dcb9984
Seb <sllabres@gmail.com>
2012-11-24 22:46:58 +0000

    Automated Commit

--- taxi/templates/taxi.js
+++ taxi/templates/taxi.js
@@ -1,5 +1,6 @@
 /*{# Copyright (c) 2010-2012 Turbulenz Limited #}*/
 
+/*{{ javascript("jslib/requesthandler.js") }}*/
 /*{{ javascript("jslib/services/mappingtable.js") }}*/
 /*{{ javascript("jslib/draw2d.js") }}*/
 
