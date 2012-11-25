324a0e359b8647d06e8b48efc92117dfa59fb682
Seb <sllabres@gmail.com>
2012-11-25 21:01:03 +0000

    Automated Commit

--- taxi/tests/RendererTests.js
+++ taxi/tests/RendererTests.js
@@ -40,8 +40,7 @@ function Renderer(turbulenzEngine, draw2d) {
 		var beginFrameCalled = false,
 			graphicsDeviceMock = { beginFrame : function () { beginFrameCalled = true; } },
 			turbulenzEngineStub = { createGraphicsDevice : function () { return graphicsDeviceMock; } },
-			drawingStub = { setBackBuffer : function () { } },
-			draw2d = { create : function () { return drawingStub; } },
+			draw2d = { create : function () { return { }; } },
 			renderer = new Renderer(turbulenzEngineStub, draw2d);
 
 		renderer.draw();
@@ -61,20 +60,6 @@ function Renderer(turbulenzEngine, draw2d) {
 		renderer.draw();
 
 		ok(setBackBufferCalled);
-	});
-
-	test("On draw when beginFrame is false, draw2d setBackBuffer not called", function () {
-		var setBackBufferCalled = false,
-			graphicsDeviceStub = { beginFrame : function () { return false; } },
-			turbulenzEngineStub = { createGraphicsDevice : function () { return graphicsDeviceStub; } },
-			drawingMock = { setBackBuffer : function () { setBackBufferCalled = true; },
-				clear : function () { } },
-			draw2dStub = { create : function () { return drawingMock; } },
-			renderer = new Renderer(turbulenzEngineStub, draw2dStub);
-
-		renderer.draw();
-
-		equal(setBackBufferCalled, false);
 	});
 
 	test("On draw and beginFrame is true, draw2d clear called", function () {
