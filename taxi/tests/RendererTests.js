4b02f34739db92a8975e22c4937c8811bd1e0a37
Seb <sllabres@gmail.com>
2012-11-25 20:38:16 +0000

    Automated Commit

--- taxi/tests/RendererTests.js
+++ taxi/tests/RendererTests.js
@@ -1,24 +1,41 @@
 /*global module, equal, test, ok*/
-var TurbulenzEngine = {};
-
-function Renderer() {
-	TurbulenzEngine.createGraphicsDevice({});
+function Renderer(turbulenzEngine, draw2d) {
+	"use strict";
+	var graphicsDevice = turbulenzEngine.createGraphicsDevice({}),
+		drwaing = draw2d.create(graphicsDevice);
 }
 
 (function () {
 	"use strict";
 	module("Renderer");
 	test("On construction, createGraphicsDevice called with params", function () {
-		var createGraphicsDeviceCalledWithParams = false;
-
-		TurbulenzEngine.createGraphicsDevice = function (params) {
-			if(!!params) {
-				createGraphicsDeviceCalledWithParams = true;
+		var createGraphicsDeviceCalledWithParams = false,
+			turbulenzEngine = { createGraphicsDevice : function (params) {
+				if (!!params) {
+					createGraphicsDeviceCalledWithParams = true;
+				}
 			}
-		};
-
-		new Renderer();
+				},
+			draw2d = { create : function (graphicsDevice) {	} },
+			renderer = new Renderer(turbulenzEngine, draw2d);
 
 		ok(createGraphicsDeviceCalledWithParams);
+	});
+
+	test("On construction, Draw2d create called with graphics device", function () {
+		var draw2dCreateCalled = false,
+			expectedGraphicsDevice = "graphisDevice",
+			passedGraphicsDevice = "",
+			turbulenzEngine = { createGraphicsDevice : function () {
+				return expectedGraphicsDevice;
+			}
+				},
+			draw2d = { create : function (graphicsDevice) {
+				passedGraphicsDevice = graphicsDevice;
+			}
+				},
+			renderer = new Renderer(turbulenzEngine, draw2d);
+
+		equal(expectedGraphicsDevice, passedGraphicsDevice);
 	});
 }());
