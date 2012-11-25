2f1f32e5724e649d87a18e30ec5ae0ced039f995
Seb <sllabres@gmail.com>
2012-11-25 20:51:36 +0000

    Automated Commit

--- taxi/tests/RendererTests.js
+++ taxi/tests/RendererTests.js
@@ -2,7 +2,14 @@
 function Renderer(turbulenzEngine, draw2d) {
 	"use strict";
 	var graphicsDevice = turbulenzEngine.createGraphicsDevice({}),
-		drwaing = draw2d.create(graphicsDevice);
+		drawing = draw2d.create(graphicsDevice);
+
+	function draw() {
+		graphicsDevice.beginFrame();
+		drawing.setBackBuffer();
+	}
+
+	return { draw : draw };
 }
 
 (function () {
@@ -10,12 +17,7 @@ function Renderer(turbulenzEngine, draw2d) {
 	module("Renderer");
 	test("On construction, createGraphicsDevice called with params", function () {
 		var createGraphicsDeviceCalledWithParams = false,
-			turbulenzEngine = { createGraphicsDevice : function (params) {
-				if (!!params) {
-					createGraphicsDeviceCalledWithParams = true;
-				}
-			}
-				},
+			turbulenzEngine = { createGraphicsDevice : function (params) { if (!!params) { createGraphicsDeviceCalledWithParams = true; } } },
 			draw2d = { create : function (graphicsDevice) {	} },
 			renderer = new Renderer(turbulenzEngine, draw2d);
 
@@ -26,16 +28,36 @@ function Renderer(turbulenzEngine, draw2d) {
 		var draw2dCreateCalled = false,
 			expectedGraphicsDevice = "graphisDevice",
 			passedGraphicsDevice = "",
-			turbulenzEngine = { createGraphicsDevice : function () {
-				return expectedGraphicsDevice;
-			}
-				},
-			draw2d = { create : function (graphicsDevice) {
-				passedGraphicsDevice = graphicsDevice;
-			}
-				},
+			turbulenzEngine = { createGraphicsDevice : function () { return expectedGraphicsDevice;	} },
+			draw2d = { create : function (graphicsDevice) { passedGraphicsDevice = graphicsDevice; } },
 			renderer = new Renderer(turbulenzEngine, draw2d);
 
 		equal(expectedGraphicsDevice, passedGraphicsDevice);
+	});
+
+	test("On draw, begin frame called", function () {
+		var beginFrameCalled = false,
+			graphicsDeviceMock = { beginFrame : function () { beginFrameCalled = true; } },			
+			turbulenzEngineStub = { createGraphicsDevice : function () { return graphicsDeviceMock; } },
+			drawingStub = { setBackBuffer : function () { } },
+			draw2d = { create : function () { return drawingStub; } },
+			renderer = new Renderer(turbulenzEngineStub, draw2d);
+
+		renderer.draw();
+
+		ok(beginFrameCalled);
+	});
+
+	test("On draw, draw2d setBackBuffer called", function () {
+		var setBackBufferCalled = false,
+			graphicsDeviceStub = { beginFrame : function () { } },			
+			turbulenzEngineStub = { createGraphicsDevice : function () { return graphicsDeviceStub; } },
+			drawingMock = { setBackBuffer : function () { setBackBufferCalled = true; } },
+			draw2dStub = { create : function () { return drawingMock; } },
+			renderer = new Renderer(turbulenzEngineStub, draw2dStub);
+
+		renderer.draw();
+
+		ok(setBackBufferCalled);
 	});
 }());
