53ae8802d931b464dd5a211c4a5aaf39b12f889c
Seb <sllabres@gmail.com>
2012-11-26 21:08:44 +0000

    Automated Commit

--- taxi/scripts/SpriteDrawing.js
+++ taxi/scripts/SpriteDrawing.js
@@ -1,9 +1,12 @@
-function SpriteDrawing(draw2d, graphicsDevice) {
+function SpriteDrawing(draw2d, graphicsDevice, jquery) {
 	"use strict";
 	var drawing = draw2d.create({ graphicsDevice : graphicsDevice });
 
 	function draw(clearColour, sprite) {
 		if (graphicsDevice.beginFrame()) {
+			if(jquery != undefined) {
+				jquery(this).trigger('beginFrame');
+			}
 			drawing.setBackBuffer();
 			drawing.clear(clearColour);
 			drawing.begin('alpha');
