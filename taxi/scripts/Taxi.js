610c884a0ad32bcefeea2b2efa5dda78db6667c9
Seb <sllabres@gmail.com>
2012-11-27 23:25:37 +0000

    Automated Commit

--- taxi/scripts/Taxi.js
+++ taxi/scripts/Taxi.js
@@ -131,7 +131,7 @@
 		var onKeyUp = function onKeyUpFn(keynum) {
 			if (keynum === keyCodes.W) {				
 				console.log("W");
-				body.setForce(1);				
+				body.setForce(0.01);				
 			}
 
 			/*if (keynum === keyCodes.D) {				
