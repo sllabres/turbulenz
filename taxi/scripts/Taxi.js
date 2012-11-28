ae9a9ff20ac03b5d25ed71eb67f769c443122637
Seb <sllabres@gmail.com>
2012-11-28 22:24:01 +0000

    Automated Commit

--- taxi/scripts/Taxi.js
+++ taxi/scripts/Taxi.js
@@ -113,7 +113,7 @@
 							background = Draw2DSprite.create({
 							origin: [0,0],
 							texture : texture,
-							height : graphicsDevice.height,
+							height : graphicsDevice.height / 2,
 							width : graphicsDevice.width
 							});							
 						}
