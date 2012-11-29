961ad7238d7bbde1e65a3882dfb04513ba54b9ad
Seb <sllabres@gmail.com>
2012-11-29 20:24:25 +0000

    Automated Commit

--- taxi/scripts/Taxi.js
+++ taxi/scripts/Taxi.js
@@ -21,7 +21,7 @@
  			stageWidth = graphicsDevice.width,
  			physics2D = Physics2DDevice.create(),
  			world = physics2D.createWorld({
-				gravity : [0, 40]
+				gravity : [0, 400]
 			}),
 			heavyMaterial = physics2D.createMaterial({
 				elasticity : 0.3,
