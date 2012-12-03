568056583b85373d1e590c0e512b449f71bc0d95
Seb <sllabres@gmail.com>
2012-12-03 19:48:07 +0000

    Automated Commit

--- taxi/tests/GameTests.js
+++ taxi/tests/GameTests.js
@@ -5,13 +5,25 @@
 	test("When created, Then createGraphicsDevice called with parameters", function() {
 		var graphicsDeviceParametersReceived = {},
 			TurbulenzEngine = { createGraphicsDevice : function(parameters) { graphicsDeviceParametersReceived = parameters } },
-			game = new Game(TurbulenzEngine);
+			Draw2D = { create : function() { } },
+			game = new Game(TurbulenzEngine, Draw2D);
 
 		ok(graphicsDeviceParametersReceived !== undefined);
-	});	
+	});
+
+	test("When created, Then Draw2D created with graphics device", function() {
+		var expectedGraphicsDevice = "graphicsDevice",
+			graphicsDeviceReceived = "",
+			TurbulenzEngine = { createGraphicsDevice : function() { return expectedGraphicsDevice; }, },
+			Draw2D = { create : function(draw2DParameters) { graphicsDeviceReceived = draw2DParameters.graphicsDevice; } },
+			game = new Game(TurbulenzEngine, Draw2D);
+
+		equal(expectedGraphicsDevice, graphicsDeviceReceived);
+	});
 }());
 
-function Game(TurbulenzEngine) {
+function Game(TurbulenzEngine, Draw2D) {
 	"use strict";
-	var graphicsDevice = TurbulenzEngine.createGraphicsDevice({ });
+	var graphicsDevice = TurbulenzEngine.createGraphicsDevice({ }),
+		Drawing2D = Draw2D.create( { graphicsDevice : graphicsDevice });	
 }
