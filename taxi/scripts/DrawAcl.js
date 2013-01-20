b8ab4dfa9d2a6a1c20be3bcae022f8a0c61f7322
Seb <sllabres@gmail.com>
2013-01-20 22:21:15 +0000

    Automated Commit

--- taxi/scripts/DrawAcl.js
+++ taxi/scripts/DrawAcl.js
@@ -2,7 +2,7 @@
 function DrawAcl(graphicsDevice) {
 	"use strict";
 	var draw2D = Draw2D.create({ graphicsDevice : graphicsDevice }),    	
-    	backgroundt;
+    	background;
 
     draw2D.configure({ viewportRectangle : [0, 0, graphicsDevice.width, graphicsDevice.height], scaleMode : 'scale' });    
 
@@ -11,14 +11,14 @@ function DrawAcl(graphicsDevice) {
 			draw2D.setBackBuffer();
 			draw2D.clear([0.3,0.3,0.3,1]);
 			draw2D.begin('alpha');			
-			draw2D.drawSprite(backgroundt);
+			draw2D.drawSprite(background);
 			draw2D.end();
 			graphicsDevice.endFrame();
 		}
     }
 
-    function setBackground(background) {
-    	backgroundt = background;
+    function setBackground(backgroundAsset) {
+    	background = backgroundAsset;
     }
 
     return { draw: draw,
