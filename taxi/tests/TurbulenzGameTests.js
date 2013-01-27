aa1b37a7f8c259b6f2d8915d02ca726f48303b40
Seb <sllabres@gmail.com>
2013-01-27 21:45:31 +0000

    Automated Commit

--- taxi/tests/TurbulenzGameTests.js
+++ taxi/tests/TurbulenzGameTests.js
@@ -12,14 +12,30 @@
 
 		ok(Object.keys(passedParameters).length === expectedPropertiesCount);
 	});
+
+	test("When onload Then create GrapihicsDevice with empty parameters", function() {
+		var passedParameters = null,
+			expectedPropertiesCount = 0,
+			requestHandlerStub = { create : function() { } },
+			turbulenzEngine = { createGraphicsDevice : function(parameters) { passedParameters = parameters; } },
+			turbulenzGame = new TurbulenzGame(requestHandlerStub, turbulenzEngine);
+
+		turbulenzGame.load();
+
+		ok(Object.keys(passedParameters).length === expectedPropertiesCount);
+	});
 }());
 
-function TurbulenzGame(requestHandlerFactory) {
+function TurbulenzGame(requestHandlerFactory, turbulenzEngine) {
 	"use strict";
 	var requestHandler = {};
 
 	function load() {
 		requestHandler = requestHandlerFactory.create({});
+
+		if(turbulenzEngine != null) {
+			turbulenzEngine.createGraphicsDevice({});
+		}
 	}
 
 	return { load : load };
