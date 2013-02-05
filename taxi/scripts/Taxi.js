8ca788f339a06ee5fe4c735a895eb55beefe474a
Seb <sllabres@gmail.com>
2013-02-05 23:18:53 +0000

    Automated Commit

--- taxi/scripts/Taxi.js
+++ taxi/scripts/Taxi.js
@@ -86,7 +86,14 @@
 			TurbulenzServices.createMappingTable(requestHandler,
 				gameSession,
 				function (table) {
-					var texture = textureManager.load(table.getURL("textures/Sky.jpg"), true, onload);					
+					var texture = textureManager.load(table.getURL("textures/Sky.jpg"), true, onload);
+
+					graphicsDevice.createTexture({ src : table.getURL("textures/Sky.jpg"),
+						mipmaps : true,						
+						onload : function (texture) {
+							alert(texture);												
+						}
+					});				
 				});
 		}			
 	};
