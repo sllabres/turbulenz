8680b3c5af810287a0657cef9d8aad1c854abd3f
Seb <sllabres@gmail.com>
2013-02-06 22:55:39 +0000

    Automated Commit

--- taxi/tests/DrawingTests.js
+++ taxi/tests/DrawingTests.js
@@ -2,14 +2,6 @@
 (function () {
 	"use strict";
 	module("Given drawing");
-	test("When constructed Then Draw2d created called", function() {
-		var draw2dCreateCalled = false,
-			draw2DFactoryMock = { create : function() { draw2dCreateCalled = true; } },
-			drawing = new Drawing(draw2DFactoryMock);
-
-		ok(draw2dCreateCalled);
-	});
-
 	test("When constructed Then Draw2d created called with GraphicsDevice", function() {
 		var expectedGraphicsDevice = "expectedGraphicsDevice",
 			receievedGraphicsDevice = "",
@@ -22,5 +14,6 @@
 }());
 
 function Drawing(draw2DFactory, graphicsDevice) {
+	"use strict";
 	draw2DFactory.create( { graphicsDevice : graphicsDevice } );
 }
