463d832ff84033d6e0f9affc30888b3ed35e862a
Seb <sllabres@gmail.com>
2013-02-06 21:43:12 +0000

    Automated Commit

--- taxi/tests/BackgroundSpriteFactoryTests.js
+++ taxi/tests/BackgroundSpriteFactoryTests.js
@@ -2,16 +2,6 @@
 (function () {
 	"use strict";
 	module("Given background sprite factory");
-	test("When create called Then Draw2DSprite.create called", function() {		
-		var draw2dSpriteCreatedCalled = false,
-			draw2dSpriteFactoryMock = { create : function() { draw2dSpriteCreatedCalled = true; } },
-			backgroundSpriteFactory = new BackgroundSpriteFactory(draw2dSpriteFactoryMock);			
-
-		backgroundSpriteFactory.create();
-
-		ok(draw2dSpriteCreatedCalled);
-	});
-
 	test("When create called Then Draw2DSprite.create called with origin [0,0]", function() {		
 		var expectedOrigin = [0,0],
 			receivedOrigin = null,
@@ -23,12 +13,24 @@
 		equal(receivedOrigin[0], expectedOrigin[0]);
 		equal(receivedOrigin[1], expectedOrigin[1]);
 	});
+
+	test("When create called with texture Then Draw2DSprite.create called with texture", function() {		
+		var expectedExpectedTexture = "expectedTexture",
+			receivedTexture = "",
+			draw2dSpriteFactoryMock = { create : function(parameters) { receivedTexture = parameters.texture; } },
+			backgroundSpriteFactory = new BackgroundSpriteFactory(draw2dSpriteFactoryMock);			
+
+		backgroundSpriteFactory.create(expectedExpectedTexture);
+
+		equal(receivedTexture, expectedExpectedTexture);
+	});
 }());
 
 function BackgroundSpriteFactory(draw2DSpriteFactory) {
 
-	function create() {
-		draw2DSpriteFactory.create({ origin: [0,0] });
+	function create(texture) {
+		draw2DSpriteFactory.create({	origin: [0,0], 
+										texture: texture });
 	}
 
 	return { create : create };	
