05670430d6d362be29afc8202c6edfb67fe9b9da
Seb <sllabres@gmail.com>
2013-02-06 19:01:33 +0000

    Automated Commit

--- taxi/tests/TextureLoaderTests.js
+++ taxi/tests/TextureLoaderTests.js
@@ -3,6 +3,21 @@
 	"use strict";
 	module("Given texture loader");
 	test("When load called Then TextureManager created", function() {
-		ok(true);
+		var textureManagerCreateCalled = false,
+			textureManagerMock = { create : function() { textureManagerCreateCalled = true; } },
+			textureLoader = new TextureLoader(textureManagerMock);
+
+		textureLoader.load();
+
+		ok(textureManagerCreateCalled);
 	});
 }());
+
+function TextureLoader(textureManager) {
+	"use strict";
+	function load() {
+		textureManager.create();
+	}
+
+	return { load : load };
+}
