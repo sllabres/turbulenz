ec4ab28ca17796298efbab05546627f792939221
Seb <sllabres@gmail.com>
2013-02-06 22:10:47 +0000

    Automated Commit

--- taxi/tests/DrawingTests.js
+++ taxi/tests/DrawingTests.js
@@ -4,9 +4,13 @@
 	module("Given drawing");
 	test("When constructed Then Draw2d created called", function() {
 		var draw2dCreateCalled = false,
-			draw2dMock = { create : function() { draw2dCreateCalled } },
-			drawing = new Drawing(draw2dMock);			
+			draw2DFactoryMock = { create : function() { draw2dCreateCalled = true; } },
+			drawing = new Drawing(draw2DFactoryMock);
 
 		ok(draw2dCreateCalled);
 	});
 }());
+
+function Drawing(draw2DFactory) {
+	draw2DFactory.create();
+}
