e9d873df5687a1dad6ed3d63a0a1ae971ec05260
Seb <sllabres@gmail.com>
2013-02-06 21:47:17 +0000

    Automated Commit

--- taxi/tests/BackgroundSpriteFactoryTests.js
+++ taxi/tests/BackgroundSpriteFactoryTests.js
@@ -5,8 +5,9 @@
 	test("When create called Then Draw2DSprite.create called with origin [0,0]", function() {		
 		var expectedOrigin = [0,0],
 			receivedOrigin = null,
+			graphicsDeviceStub = { height: 0 },
 			draw2dSpriteFactoryMock = { create : function(parameters) { receivedOrigin = parameters.origin; } },
-			backgroundSpriteFactory = new BackgroundSpriteFactory(draw2dSpriteFactoryMock);			
+			backgroundSpriteFactory = new BackgroundSpriteFactory(draw2dSpriteFactoryMock, graphicsDeviceStub);			
 
 		backgroundSpriteFactory.create();
 
@@ -17,20 +18,34 @@
 	test("When create called with texture Then Draw2DSprite.create called with texture", function() {		
 		var expectedExpectedTexture = "expectedTexture",
 			receivedTexture = "",
+			graphicsDeviceStub = { height: 0 },
 			draw2dSpriteFactoryMock = { create : function(parameters) { receivedTexture = parameters.texture; } },
-			backgroundSpriteFactory = new BackgroundSpriteFactory(draw2dSpriteFactoryMock);			
+			backgroundSpriteFactory = new BackgroundSpriteFactory(draw2dSpriteFactoryMock, graphicsDeviceStub);			
 
 		backgroundSpriteFactory.create(expectedExpectedTexture);
 
 		equal(receivedTexture, expectedExpectedTexture);
 	});
+
+	test("When create called Then Draw2DSprite.create called with screen height", function() {		
+		var expectedExpectedHeight = 100,
+			receivedHeight = 0,
+			graphicsDeviceStub = { height: 100 },
+			draw2dSpriteFactoryMock = { create : function(parameters) { receivedHeight = parameters.height; } },
+			backgroundSpriteFactory = new BackgroundSpriteFactory(draw2dSpriteFactoryMock, graphicsDeviceStub);			
+
+		backgroundSpriteFactory.create();
+
+		equal(receivedHeight, expectedExpectedHeight);
+	});
 }());
 
-function BackgroundSpriteFactory(draw2DSpriteFactory) {
+function BackgroundSpriteFactory(draw2DSpriteFactory, graphicsDevice) {
 
 	function create(texture) {
 		draw2DSpriteFactory.create({	origin: [0,0], 
-										texture: texture });
+										texture: texture,
+										height: graphicsDevice.height });
 	}
 
 	return { create : create };	
