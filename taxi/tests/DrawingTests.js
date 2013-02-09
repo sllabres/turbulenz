95c1337908bd85854b5d148b4c5b46b7fa4ff601
Seb <sllabres@gmail.com>
2013-02-09 23:42:35 +0000

    Automated Commit

--- taxi/tests/DrawingTests.js
+++ taxi/tests/DrawingTests.js
@@ -47,55 +47,6 @@
 		ok(draw2DClearCalled);
 	});
 
-	// 0.3,0.3,0.3,1
-	test("When draw called and graphicsDevice.BeginFrame returns true Then draw2D.clear called with red value of 0.3", function() {
-		var expectedClearRedValue = 0.3,
-			receievedClearRedValue = 0,
-			graphicsDeviceStub = { beginFrame : function() { return true; } },
-			draw2DMock = { begin : function(argument) { }, clear : function(colour) { receievedClearRedValue = colour[0]; } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
-
-		drawing.draw();
-
-		equal(receievedClearRedValue, expectedClearRedValue);
-	});
-
-	test("When draw called and graphicsDevice.BeginFrame returns true Then draw2D.clear called with green value of 0.3", function() {
-		var expectedClearGreenValue = 0.3,
-			receievedClearGreenValue = 0,
-			graphicsDeviceStub = { beginFrame : function() { return true; } },
-			draw2DMock = { begin : function(argument) { }, clear : function(colour) { receievedClearGreenValue = colour[1]; } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
-
-		drawing.draw();
-
-		equal(receievedClearGreenValue, expectedClearGreenValue);
-	});
-
-	test("When draw called and graphicsDevice.BeginFrame returns true Then draw2D.clear called with blue value of 0.3", function() {
-		var expectedClearBlueValue = 0.3,
-			receievedClearBlueValue = 0,
-			graphicsDeviceStub = { beginFrame : function() { return true; } },
-			draw2DMock = { begin : function(argument) { }, clear : function(colour) { receievedClearBlueValue = colour[2]; } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
-
-		drawing.draw();
-
-		equal(receievedClearBlueValue, expectedClearBlueValue);
-	});
-
-	test("When draw called and graphicsDevice.BeginFrame returns true Then draw2D.clear called with alpha value of 1", function() {
-		var expectedClearAlphaValue = 1,
-			receievedClearAlphaValue = 0,
-			graphicsDeviceStub = { beginFrame : function() { return true; } },
-			draw2DMock = { begin : function(argument) { }, clear : function(colour) { receievedClearAlphaValue = colour[3]; } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
-
-		drawing.draw();
-
-		equal(receievedClearAlphaValue, expectedClearAlphaValue);
-	});
-
 	test("When draw called with clearColour and graphicsDevice.BeginFrame returns true Then draw2D.clear called with passed in clearColour", function() {
 		var expectedClearColour = [0.3,0.3,0.3,1],
 			receievedClearClearColour = [],
@@ -115,12 +66,7 @@ function Drawing(graphicsDevice, draw2D) {
 	function draw(clearColour) {
 		if(graphicsDevice.beginFrame()) {
 			draw2D.begin('alpha');
-			if(clearColour === undefined) {
-				draw2D.clear([0.3, 0.3, 0.3, 1]);
-			} else {
-				draw2D.clear(clearColour);
-			}
-
+			draw2D.clear(clearColour);
 		}
 	}
 
