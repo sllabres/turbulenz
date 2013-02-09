094daaac52da240e43acac981fd66238caeeef9a
Seb <sllabres@gmail.com>
2013-02-09 23:26:18 +0000

    Automated Commit

--- taxi/tests/DrawingTests.js
+++ taxi/tests/DrawingTests.js
@@ -11,12 +11,27 @@
 
 		ok(beginFrameCalled);
 	});
+
+	test("When draw called and graphicsDevice.BeginFrame returns true Then draw2D.begin called", function() {
+		var draw2DBeginCalled = false,
+			graphicsDeviceStub = { beginFrame : function() { return true; } },
+			draw2DMock = { begin : function() { draw2DBeginCalled = true; } },
+			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
+
+		drawing.draw();
+
+		ok(draw2DBeginCalled);
+	});
 }());
 
-function Drawing(graphicsDevice) {
+function Drawing(graphicsDevice, draw2D) {
 	"use strict";
+
 	function draw() {
 		graphicsDevice.beginFrame();	
+		if(draw2D != undefined) {
+			draw2D.begin();
+		}
 	}
 
 	return { draw : draw };
