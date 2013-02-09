ce393391c334a1b27e9f04b91ed49c3233adab5a
Seb <sllabres@gmail.com>
2013-02-09 23:56:51 +0000

    Automated Commit

--- taxi/tests/DrawingTests.js
+++ taxi/tests/DrawingTests.js
@@ -4,9 +4,10 @@
 	module("Given drawing");
 	test("When draw called and graphicsDevice.BeginFrame returns true Then draw2D.begin called", function() {
 		var draw2DBeginCalled = false,
+			drawingObserverStub = { subscribe : function(type) { } },
 			graphicsDeviceStub = { beginFrame : function() { return true; } },
 			draw2DMock = { begin : function() { draw2DBeginCalled = true; }, clear : function() { }, setBackBuffer : function() { } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
+			drawing = new Drawing(graphicsDeviceStub, draw2DMock, drawingObserverStub);
 
 		drawing.draw();
 
@@ -15,9 +16,10 @@
 
 	test("When draw called and graphicsDevice.BeginFrame returns false Then draw2D.begin NOT called", function() {
 		var draw2DBeginCalled = false,
+			drawingObserverStub = { subscribe : function(type) { } },
 			graphicsDeviceStub = { beginFrame : function() { return false; } },
 			draw2DMock = { begin : function() { draw2DBeginCalled = true; }, clear : function() { }, setBackBuffer : function() { } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
+			drawing = new Drawing(graphicsDeviceStub, draw2DMock, drawingObserverStub);
 
 		drawing.draw();
 
@@ -27,9 +29,10 @@
 	test("When draw called and graphicsDevice.BeginFrame returns true Then draw2D.begin called with argument 'alpha'", function() {
 		var expectedArgument = "alpha",
 			receivedArgument = "",
+			drawingObserverStub = { subscribe : function(type) { } },
 			graphicsDeviceStub = { beginFrame : function() { return true; } },
 			draw2DMock = { begin : function(argument) { receivedArgument = argument; }, clear : function() { }, setBackBuffer : function() { } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
+			drawing = new Drawing(graphicsDeviceStub, draw2DMock, drawingObserverStub);
 
 		drawing.draw();
 
@@ -37,10 +40,11 @@
 	});
 
 	test("When draw called and graphicsDevice.BeginFrame returns true Then draw2D.clear called", function() {
-		var draw2DClearCalled = false,			
+		var draw2DClearCalled = false,
+			drawingObserverStub = { subscribe : function(type) { } },
 			graphicsDeviceStub = { beginFrame : function() { return true; } },
 			draw2DMock = { begin : function(argument) { }, clear : function() { draw2DClearCalled = true; }, setBackBuffer : function() { } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
+			drawing = new Drawing(graphicsDeviceStub, draw2DMock, drawingObserverStub);
 
 		drawing.draw();
 
@@ -50,9 +54,10 @@
 	test("When draw called with clearColour and graphicsDevice.BeginFrame returns true Then draw2D.clear called with passed in clearColour", function() {
 		var expectedClearColour = [0.3,0.3,0.3,1],
 			receievedClearClearColour = [],
+			drawingObserverStub = { subscribe : function(type) { } },
 			graphicsDeviceStub = { beginFrame : function() { return true; } },
 			draw2DMock = { begin : function(argument) { }, clear : function(colour) { receievedClearClearColour = colour; }, setBackBuffer : function() { } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
+			drawing = new Drawing(graphicsDeviceStub, draw2DMock, drawingObserverStub);
 
 		drawing.draw(expectedClearColour);
 
@@ -61,13 +66,27 @@
 
 	test("When draw called and graphicsDevice.BeginFrame returns true Then draw2D.setBackBuffer called", function() {
 		var setBackBufferCalled = false,
+			drawingObserverStub = { subscribe : function(type) { } },
 			graphicsDeviceStub = { beginFrame : function() { return true; } },
 			draw2DMock = { begin : function(argument) { }, clear : function(colour) { }, setBackBuffer : function() { setBackBufferCalled = true; } },
-			drawing = new Drawing(graphicsDeviceStub, draw2DMock);
+			drawing = new Drawing(graphicsDeviceStub, draw2DMock, drawingObserverStub);
 
 		drawing.draw({ });
 
 		ok(setBackBufferCalled);
+	});
+
+	test("When draw called and drawing prepare complete Then drawingPrepareComplete notification sent", function() {
+		var expectedNotification = "drawingPrepareComplete",
+			receivedNotifcation = "",
+			drawingObserverMock = { subscribe : function(type) { }, notify : function(type) { receivedNotifcation = type; } },
+			graphicsDeviceStub = { beginFrame : function() { return true; } },
+			draw2DMock = { begin : function(argument) { }, clear : function(colour) { }, setBackBuffer : function() { } },
+			drawing = new Drawing(graphicsDeviceStub, draw2DMock, drawingObserverMock);
+
+		drawing.draw({ });
+
+		ok(receivedNotifcation, expectedNotification);
 	});
 
 	test("When draw called Then drawingPrepareComplete event subscribed to", function() {
@@ -91,7 +110,11 @@ function Drawing(graphicsDevice, draw2D, drawingObserver) {
 		if(graphicsDevice.beginFrame()) {
 			draw2D.begin('alpha');
 			draw2D.clear(clearColour);			
-			draw2D.setBackBuffer();			
+			draw2D.setBackBuffer();
+			
+			if(drawingObserver.notify !== undefined) {
+				drawingObserver.notify("drawingPrepareComplete");
+			}
 		}
 	}
 
