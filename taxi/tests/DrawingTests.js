9a8a0b50a47332543f821185674b923baca446d7
Seb <sllabres@gmail.com>
2013-02-10 16:28:48 +0000

    Automated Commit

--- taxi/tests/DrawingTests.js
+++ taxi/tests/DrawingTests.js
@@ -2,7 +2,24 @@
 (function () {
 	"use strict";
 	module("Given drawing");
-	test("", function() {
-		ok(true);
+	test("When calling draw Then draw2D.drawSprite called", function() {
+		var drawSpriteCalled = false,
+			draw2DMock = { drawSprite : function() { drawSpriteCalled = true; } },
+			drawing = new Drawing(draw2DMock);
+
+		drawing.draw();
+
+		ok(drawSpriteCalled);
 	});
 }());
+
+//draw2D.drawSprite(background);
+
+function Drawing(draw2D) { 
+
+	function draw() {
+		draw2D.drawSprite();
+	}
+
+	return { draw : draw };
+}
