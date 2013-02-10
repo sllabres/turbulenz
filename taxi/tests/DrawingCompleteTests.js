b0bdddc1ec2a3ec4ff23054bf84ae3c5f4072ed3
Seb <sllabres@gmail.com>
2013-02-10 16:40:47 +0000

    Automated Commit

--- taxi/tests/DrawingCompleteTests.js
+++ taxi/tests/DrawingCompleteTests.js
@@ -2,7 +2,16 @@
 (function () {
 	"use strict";
 	module("Given drawing complete");
-	test("When instantiating then subscribes to drawingComplete event type", function() {
-		ok(true);
+	test("When instantiating then subscribes to drawSpriteComplete event type", function() {
+		var expectedEventSubscribe = "drawSpriteComplete",
+			receivedEventSubscribe = "",			
+			observerMock = { subscribe : function(type) { receivedEventSubscribe = type; } },
+			drawing = new DrawingComplete(observerMock);
+
+		equal(receivedEventSubscribe, expectedEventSubscribe);
 	});
 }());
+
+function DrawingComplete(observer) {
+	observer.subscribe("drawSpriteComplete");
+}
