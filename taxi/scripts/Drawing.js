e3630a4ccc11665389511e6f1d71fc440daf65a0
Seb <sllabres@gmail.com>
2013-02-10 19:21:54 +0000

    Automated Commit

--- taxi/scripts/Drawing.js
+++ taxi/scripts/Drawing.js
@@ -1,8 +1,26 @@
-function Drawing(observer, draw2D) {
-	"use strict";
-	observer.subscribe("drawingPrepareComplete", draw);
+function Drawing(graphicsDevice, draw2D, observer) {
+	"use strict";		
+	observer.subscribe("drawingPrepareComplete", drawSprite);
+	observer.subscribe("drawSpriteComplete", complete);
 
-	function draw() {
-		draw2D.drawSprite();
+	function prepare(clearColour) {
+		if(graphicsDevice.beginFrame()) {
+			draw2D.begin('alpha');
+			draw2D.clear(clearColour);			
+			draw2D.setBackBuffer();
+			observer.notify("drawingPrepareComplete");			
+		}
 	}
+
+	function drawSprite() {
+		draw2D.drawSprite();
+		observer.notify("drawSpriteComplete");		
+	}
+
+	function complete() {
+		draw2D.end();		
+		graphicsDevice.endFrame();		
+	}
+
+	return { prepare : prepare };
 }
