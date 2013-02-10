59967dc919dff0fe22eabec5729b984ee02c559f
Seb <sllabres@gmail.com>
2013-02-10 09:10:56 +0000

    Automated Commit

--- taxi/tests/GameTests.js
+++ taxi/tests/GameTests.js
@@ -8,35 +8,35 @@
 			requestHandlerFactoryMock = { create : function(parameters) { passedParameters = parameters; } },
 			turbulenzEngineStub = { createGraphicsDevice : function() { } },
 			turbulenzServicesStub = { createGameSession : function() { } },
-			turbulenzGame = new Game(requestHandlerFactoryMock);
+			turbulenzGame = new Game(requestHandlerFactoryMock, turbulenzEngineStub);
 
 		turbulenzGame.load();
 
 		ok(Object.keys(passedParameters).length === expectedPropertiesCount);
 	});
 
-	/*test("When load called Then create GrapihicsDevice with empty parameters", function() {
+	test("When load called Then create GrapihicsDevice with empty parameters", function() {
 		var passedParameters = null,
 			expectedPropertiesCount = 0,			
 			requestHandlerFactoryStub = { create : function() { } },
 			turbulenzEngineMock = { createGraphicsDevice : function(parameters) { passedParameters = parameters; } },
 			turbulenzServicesStub = { createGameSession : function() { } },
-			turbulenzGame = new TurbulenzGameLoader(requestHandlerFactoryStub, turbulenzEngineMock, turbulenzServicesStub);
+			turbulenzGame = new Game(requestHandlerFactoryStub, turbulenzEngineMock);
 
 		turbulenzGame.load();
 
 		ok(Object.keys(passedParameters).length === expectedPropertiesCount);
-	});*/
+	});
 }());
 
-function Game(requestHandlerFactory) {
+function Game(requestHandlerFactory, turbulenzEngine) {
 	"use strict";
 	var requestHandler = null,
 		graphicsDevice = null;
 
 	function load() {		
 		requestHandler = requestHandlerFactory.create({});
-		//graphicsDevice = turbulenzEngine.createGraphicsDevice({});
+		graphicsDevice = turbulenzEngine.createGraphicsDevice({});
 	}
 
 	return { load : load };
