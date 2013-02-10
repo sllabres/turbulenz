ab7fce45839c609972cbbab39167e2eb8fae122d
Seb <sllabres@gmail.com>
2013-02-10 16:44:45 +0000

    Automated Commit

--- taxi/tests/DrawingCompleteTests.js
+++ taxi/tests/DrawingCompleteTests.js
@@ -2,16 +2,39 @@
 (function () {
 	"use strict";
 	module("Given drawing complete");
-	test("When instantiating then subscribes to drawSpriteComplete event type", function() {
+	test("When setup called then subscribes to drawSpriteComplete event type", function() {
 		var expectedEventSubscribe = "drawSpriteComplete",
 			receivedEventSubscribe = "",			
 			observerMock = { subscribe : function(type) { receivedEventSubscribe = type; } },
-			drawing = new DrawingComplete(observerMock);
+			drawingComplete = new DrawingComplete(observerMock);
+
+		drawingComplete.setup();
 
 		equal(receivedEventSubscribe, expectedEventSubscribe);
 	});
+
+	test("When drawSpriteComplete event triggered then draw2D.end called", function() {
+		var endCalled = false,
+			subscriberObject = null,
+			draw2DMock = { end : function() { endCalled = true; } },
+			observerMock = { subscribe : function(type, subscriber) { subscriberObject = subscriber; } },
+			drawingComplete = new DrawingComplete(observerMock, draw2DMock);
+
+			drawingComplete.setup();
+			subscriberObject();
+
+		ok(endCalled);
+	});
 }());
 
-function DrawingComplete(observer) {
-	observer.subscribe("drawSpriteComplete");
+function DrawingComplete(observer, draw2D) {
+	function setup() {
+		observer.subscribe("drawSpriteComplete", complete);
+	}
+
+	function complete() {
+		draw2D.end();
+	}
+
+	return { setup : setup };
 }
