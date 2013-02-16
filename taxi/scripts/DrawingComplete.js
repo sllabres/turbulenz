f0617370c5e33b316a536a1ef0d3a0a2f88dbe70
Seb <sllabres@gmail.com>
2013-02-16 17:48:45 +0000

    Remove deleted files

--- taxi/scripts/DrawingComplete.js
+++
@@ -1,9 +0,0 @@
-function DrawingComplete(observer, draw2D, graphicsDevice) {
-	"use strict";	
-	observer.subscribe("drawSpriteComplete", complete);	
-
-	function complete() {
-		draw2D.end();		
-		graphicsDevice.endFrame();		
-	}
-}
