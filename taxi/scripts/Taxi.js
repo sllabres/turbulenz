f8222ad00231ba272ad997750065f152f6272cd4
Seb <sllabres@gmail.com>
2013-02-23 14:56:44 +0000

    Automated Commit

--- taxi/scripts/Taxi.js
+++ taxi/scripts/Taxi.js
@@ -4,7 +4,7 @@
 	TurbulenzEngine.onload = function onload() { 				
 		
 		var requestHandler = RequestHandler.create({}),
-			assetTracker = AssetTracker.create(1, true),			
+			assetTracker = AssetTracker.create(3, true),			
 			graphicsDevice = TurbulenzEngine.createGraphicsDevice({}),
 			draw2D = Draw2D.create({ graphicsDevice : graphicsDevice }),			
 			eventObserver = new EventObserver(),
