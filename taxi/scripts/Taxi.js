2c74729fcc5a6f0678f8f9333d34694885ceb472
Seb <sllabres@gmail.com>
2013-02-24 21:01:03 +0000

    Automated Commit

--- taxi/scripts/Taxi.js
+++ taxi/scripts/Taxi.js
@@ -1,7 +1,6 @@
  /*jslint browser: true*/
 /*global TurbulenzEngine,TurbulenzServices, RequestHandler*/
-(function() {
-	var ASSET_COUNT = 3;
+(function() {	
 	TurbulenzEngine.onload = function onload() {
 		var requestHandler = RequestHandler.create({}),
 			graphicsDevice = TurbulenzEngine.createGraphicsDevice({}),
