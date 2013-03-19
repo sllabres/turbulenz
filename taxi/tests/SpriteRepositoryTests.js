af27584876a280a5b692c0f3d05bcd6476838924
Seb <sllabres@gmail.com>
2013-03-19 20:45:26 +0000

    Automated Commit

--- taxi/tests/SpriteRepositoryTests.js
+++ taxi/tests/SpriteRepositoryTests.js
@@ -9,6 +9,15 @@
 		ok(subscribed);
 	});
 
+	test("When creating sprite repository Then subscribe to 'spriteLoaded' event", function() {
+		var expectedSubcribeEventName = "spriteLoaded",
+			subscribedEvent = "",
+			eventObserver = { subscribe : function(eventName) { subscribedEvent = eventName; } },
+			spriteRepository = new SpriteRepository(eventObserver);
+
+		equal(subscribedEvent, expectedSubcribeEventName);
+	});
+
 	// test("When calling getSpriteBy name Then sprite is returned", function() {
 	// 	var expectedSprite = "expectedSprite",
 	// 		eventObserver = { notify : function() { }, subscribe },
@@ -21,6 +30,6 @@
 }());
 
 function SpriteRepository(subscriber) {
-	subscriber.subscribe();
+	subscriber.subscribe("spriteLoaded");
 
 }
